de570c6f2a14c75a3d46de96dabd40036e734048
diondrapeck <diondra.d.peck@gmail.com>
2015-12-20 21:53:31 -0500

    Add counting segment

--- colony_characterization.m
+++ colony_characterization.m
@@ -9,3 +9,12 @@ z = rgb2gray(I);
 filtered = dishfilter(z);
 
 segmented = colonysegmentation(BW2, z);
+
+[label, NUM] = bwlabeln(BW3); % returns the number of colonies
+message = sprintf('There are %d colonies.', NUM); 
+h = msgbox(message); % displays number of colonies found
+
+objects1 = regionprops(BW3, z, 'Area','Eccentricity','Perimeter'); % extract properties of each colony
+
+t = struct2table(objects1); 
+writetable(t, 'colonyprops.xls'); % write property info to excel
