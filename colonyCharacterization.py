78e963be51dff472bbbc7d3c716a154da78c873b
diondrapeck <diondra.d.peck@gmail.com>
2016-01-22 19:50:49 -0500

    Add filtration and labeling

--- colonyCharacterization.py
+++ colonyCharacterization.py
@@ -8,19 +8,35 @@
 import numpy as np
 import pylab
 import mahotas as mh
+from matplotlib import pyplot as plt
 
-## Read and filter image using Otsu's thresholding
+## Read and apply gaussian filter
 I = mh.imread('file.jpeg')
-pylab.imshow(I)
-pylab.gray()
+I = np.invert(I)
+pylab.imshow(I, cmap = pylab.gray())
 pylab.show()
 
-I_filtered = mh.gaussian_filter(I, 8)
-thresh = mh.thresholding.otsu(I_filterd)
-pylab.imshow(I_filtered > thresh)
+I_filt = mh.gaussian_filter(I, 2)
+I_filt = (I_filt > I_filt.mean())
+pylab.imshow(I_filt)
 pylab.show()
 
-## 
+## Label all connected components
+labeled, objects = mh.label(I_filt)
+
+## Separate overlapping colonies
+
+
+## Remove non-colony objects (e.g. petri dish)
+relabeled, colonies = mh.labeled.filter_labeled(labeled, remove_bordering = True, min_size = 7, max_size = 12500)
+pylab.imshow(relabeled)
+pylab.show()
+
+
+
+
+
+
 
 
 
